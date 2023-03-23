a5c88a373f48c7abc91b65237c95414bfb9a70ae
bbakjjii <jiyoung920301@gmail.com>
2023-03-23 09:58:44 +0900

    2023.03.23 GitStudy Commit!

---
+++ src/fruit/Apple.java
@@ -0,0 +1,16 @@
+package fruit;
+
+public class Apple {
+	int price;
+	int grade;
+	
+	boolean clean;
+	
+	public void ate() {
+		price -= 800;
+	}
+	
+	public void clean() {
+		clean = true;
+	}
+}
